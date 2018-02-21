c1eba1715fbc8e0a659ad832de7a9141272c9d15
kotenei <kotenei@qq.com>
2018-02-21 22:54:52 +0800

    update

--- src/components/AutoComplete/index.js
+++ src/components/AutoComplete/index.js
@@ -5,7 +5,7 @@ import Input from "../Input";
 import { kClass, kSize, getClassSet } from "../../utils/kUtils";
 import { Sizes } from "../../utils/styleMaps";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
-import MultipleList from '../MultipleList';
+import MultipleList from "../MultipleList";
 
 const prefixCls = "k-autocomplete";
 
@@ -54,20 +54,113 @@ class AutoComplete extends Component {
             return item;
         }
     };
-    getSelectedList() {
-        let items = [];
+    handleKeyUp = e => {
+        const { target, keyCode } = e;
+        const { url, data } = this.props;
+        let val = target.value.trim();
 
-        return items;
+        if (!this.cache || this.cache != val) {
+            this.cache = val;
+            this.active = 0;
+            this.search(val);
+        }
+
+        switch (keyCode) {
+            case KEY.UP:
+            case KEY.LEFT:
+                break;
+            case KEY.DOWN:
+            case KEY.RIGHT:
+                break;
+            case KEY.ENTER:
+            case KEY.TAB:
+                this.select();
+                break;
+            default:
+                break;
+        }
+    };
+    //上一条
+    prev() {}
+    //下一条
+    next() {}
+    //选中
+    select() {}
+    //查询
+    search(val) {
+        const { url } = this.props;
+        if (val.length == 0) {
+            return;
+        }
+        if (url) {
+        } else {
+            let data = this.getData(val);
+            
+        }
     }
+    //取数据
+    getData(val) {
+        this.cacheData = [];
+        const { data, formatItem, max } = this.props;
+        let flag = 0,
+            ret = [];
+        if (val.length == 0) {
+            return ret;
+        }
+        for (let i = 0; i < data.length; i++) {
+            const item = data[i];
+            let formatted = formatItem(item),
+                text;
+            if (typeof formatted !== "object") {
+                text = formatted.toString().toLowerCase();
+            } else {
+                text = formatted.text.toLowerCase();
+            }
+            if (text.indexOf(val.toLowerCase()) >= 0) {
+                this.cacheData.push(item);
+                ret.push(item);
+                if (flag == max - 1) {
+                    break;
+                }
+                flag++;
+            }
+        }
+        return ret;
+    }
+    //高亮
+    highlight(char, str) {
+        const { highlight } = this.props;
+        if (highlight) {
+            let ret = new RegExp(`(${char})`, "ig");
+            str = str.replace(reg, `<strong>$1</strong>`);
+        }
+        return str;
+    }
+    //显示
+    show() {}
+    //隐藏
+    hide() {}
     renderContainer() {
         const { mode, placeholder, kSize } = this.props;
         const { value } = this.state;
         if (mode == "single") {
             return (
-                <Input type="text" kSize={kSize} placeholder={placeholder} />
+                <Input
+                    type="text"
+                    kSize={kSize}
+                    placeholder={placeholder}
+                    onKeyUp={this.handleKeyUp}
+                />
             );
         } else {
-            return <MultipleList  kSize={kSize} showInput={true} placeholder={placeholder} />
+            return (
+                <MultipleList
+                    kSize={kSize}
+                    showInput={true}
+                    placeholder={placeholder}
+                    value={value}
+                />
+            );
         }
     }
     render() {

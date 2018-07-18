079b58d245bc3d6f1d0f1485a63aa47c15f433f1
kotenei <kotenei@qq.com>
2018-07-18 23:31:56 +0800

    add timepicker

---
+++ src/components/TimePicker/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class TimePicker extends Component {
+    static propTypes = {};
+    static defaultProps = {};
+    render() {
+        return null;
+    }
+}
+
+export default TimePicker;

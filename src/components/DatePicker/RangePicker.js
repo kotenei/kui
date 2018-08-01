db3bebe784980aa6bd0079dc3f24e2ceb184297a
kotenei <kotenei@qq.com>
2018-08-01 23:36:31 +0800

    update

---
+++ src/components/DatePicker/RangePicker.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class RangePicker extends Component {
+    render() {
+        return null;
+    }
+}
+
+export default RangePicker;

cad048177e2d32108781a696a9c40e1fd4e1c3ea
kotenei <kotenei@qq.com>
2019-01-28 12:49:33 +0800

    update

---
+++ src/components/List/index.js
@@ -0,0 +1,8 @@
+import List from "./List";
+import ListItem from "./List.Item";
+import ListItemMeta from "./List.Item.Meta";
+
+List.Item = ListItem;
+List.ItemMeta = ListItemMeta;
+
+export default List;

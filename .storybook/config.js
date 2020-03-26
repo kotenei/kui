86334b6a6044971a133e635d753f9c2d2d4b4b44
kotenei <kotenei@qq.com>
2020-03-26 18:09:04 +0800

    add storybook config files

---
+++ .storybook/config.js
@@ -0,0 +1,39 @@
+import { configure, addParameters, addDecorator } from '@storybook/react';
+import { addReadme, configureReadme } from 'storybook-readme';
+
+const req = require.context('../stories', true, /\.js$/);
+
+function loadStories() {
+  req
+    .keys()
+    .sort()
+    .forEach(filename => req(filename));
+}
+
+// configureReadme({
+//   StoryPreview: StoryWrapper,
+//   DocPreview: DocWrapper,
+//   HeaderPreview: HeaderWrapper,
+//   FooterPreview: FooterWrapper
+// });
+
+addParameters({
+  options: {
+    name: 'kui-react',
+    goFullScreen: false,
+    showAddonsPanel: true,
+    showSearchBox: true,
+    addonPanelInRight: true,
+    sortStoriesByKind: true,
+    hierarchySeparator: /\./,
+    hierarchyRootSeparator: /\|/,
+    enableShortcuts: true,
+  },
+  readme: {
+    codeTheme: 'hopscotch',
+  },
+});
+
+addDecorator(addReadme);
+
+configure(loadStories, module);

b13c6dde6f7cbf27697e14e4647bdd2cadb1af11
Rain <kotenei@qq.com>
2023-08-09 14:44:36 +0800

    update

--- scripts/build.js
+++ scripts/build.js
@@ -10,10 +10,15 @@ const removeLib = () => {
   rimraf.sync(`${cwd}/lib/`);
 };
 
+const removeESM = () => {
+  console.log(chalk.cyan('Removing exiting esm folder...'));
+  rimraf.sync(`${cwd}/esm/`);
+};
+
 const complieScripts = () => {
   console.log(chalk.cyan('Compiling script files with TypeScript...'));
   shell.exec('tsc -p tsconfig-build.json');
-  // shell.exec('tsc --declaration --declarationDir types');
+  shell.exec('tsc -m esNext --outDir esm');
 
   // console.log(chalk.cyan('Compiling script files with Babel...'));
   // shell.exec('babel lib -d lib');
@@ -24,11 +29,13 @@ const complieStyles = () => {
   shell.exec(
     'node-sass src/assets/styles/index.scss lib/kui-react.min.css  --output-style compressed -q',
   );
+  shell.cp('./lib/kui-react.min.css','./esm/')
 };
 
 const build = () => {
   console.log(chalk.bgBlue(chalk.black('\n KUI BUILD START \n')));
   removeLib();
+  removeESM();
   complieScripts();
   complieStyles();
   console.log(chalk.bgGreen(chalk.black('\n KUI BUILD DONE \n')));
